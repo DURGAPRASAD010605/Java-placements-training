a5f81f6023a38b9b56e021a2d4bd860076a0cbfa
Durga Prasad <durgaprasad010605@gmail.com>
2026-04-21 10:11:45 +0530

    Day-2

---
+++ Day-2/loops2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class loops2{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a Number: ");
+        int b=sc.nextInt();
+        int i=1;
+        while(i<=5){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
